913402de0b039f73a221756db7437fc677ee4baa
Bernard Aboba <bernard.aboba@gmail.com>
2022-10-17 22:45:42 -0700

    Fix HEVC codec string

--- samples/encode-decode-worker/js/main.js
+++ samples/encode-decode-worker/js/main.js
@@ -255,7 +255,7 @@ document.addEventListener('DOMContentLoaded', async function(event) {
           config.pt = 1;
           break;
         case "H265":
-          config.codec = "hvc1.1.6.L123.00" // Main profile
+          config.codec = "hvc1.1.6.L123.00"  // Main profile, level 4.1, main Tier
           config.hevc = { format: "annexb" };
           config.pt = 2;
           addToEventLog('HEVC Encoding not supported', 'fatal');
