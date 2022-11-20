c4e1bd1190585d7790b328d07c22ae2a03a34363
Bernard Aboba <bernard.aboba@gmail.com>
2022-11-19 21:24:16 -0800

    Adjust default bitrate

--- samples/encode-decode-worker/js/main.js
+++ samples/encode-decode-worker/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let preferredResolution;
-let mediaStream, bitrate = 3000000;
+let mediaStream, bitrate = 100000;
 let stopped = false;
 let preferredCodec ="VP8";
 let mode = "L1T3";
