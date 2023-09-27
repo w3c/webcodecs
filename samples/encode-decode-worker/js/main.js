750858222d8dd13a9b4eab7177f25f2569123417
StaZhu <zhusidayoyo@hotmail.com>
2023-09-27 14:12:13 +0800

    Do not use slow CDN and change default configs for encode-decode-worker page

--- samples/encode-decode-worker/js/main.js
+++ samples/encode-decode-worker/js/main.js
@@ -3,8 +3,8 @@
 let preferredResolution;
 let mediaStream, bitrate = 100000;
 let stopped = false;
-let preferredCodec ="VP8";
-let mode = "L1T3";
+let preferredCodec ="H264";
+let mode = "L1T1";
 let latencyPref = "realtime", bitPref = "variable";
 let encHw = "no-preference", decHw = "no-preference";
 let streamWorker;
